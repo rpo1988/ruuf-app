60d709d6dd5205bc284aca3d843877aa6ae5dcd2
Rodrigo O <rpo1988@gmail.com>
2024-05-06 12:34:04 -0400

    init development

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,313 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { FormEvent, useEffect, useRef, useState } from "react";
+import { hexRandomColor } from "./utils";
+
+interface InnerRectangles {
+  horizontal: number;
+  items: {
+    height: number;
+    left: number;
+    top: number;
+    width: number;
+  }[];
+  total: number;
+  vertical: number;
+}
+
+const calculateRectangles = (
+  outerWidth: number,
+  outerHeight: number,
+  innerWidth: number,
+  innerHeight: number
+): InnerRectangles => {
+  const outerLonger = Math.max(outerWidth, outerHeight);
+  const outerShorter = Math.min(outerWidth, outerHeight);
+  const innerLonger = Math.max(innerWidth, innerHeight);
+  const innerShorter = Math.min(innerWidth, innerHeight);
+
+  if (
+    outerLonger < innerLonger ||
+    outerHeight < innerHeight ||
+    [outerWidth, outerHeight, innerWidth, innerHeight].some((item) => item <= 0)
+  ) {
+    // Inner rectangle is bigger than outer rectangle or some side is 0
+    return { total: 0, horizontal: 0, vertical: 0, items: [] };
+  }
+
+  const xHorizontalCounter = Math.floor(outerLonger / innerLonger);
+  const yHorizontalCounter = Math.floor(outerShorter / innerShorter);
+  const horizontalCounter = xHorizontalCounter * yHorizontalCounter;
+
+  // Calculate if in remaining space fit more rectangles in different position
+  const xRemaingSpace = outerLonger - xHorizontalCounter * innerLonger;
+  const verticalCounter = Math.floor(xRemaingSpace / innerShorter);
+
+  const items: {
+    height: number;
+    left: number;
+    top: number;
+    width: number;
+  }[] = [];
+  for (let xIndex = 0; xIndex < xHorizontalCounter; xIndex++) {
+    for (let yIndex = 0; yIndex < yHorizontalCounter; yIndex++) {
+      items.push({
+        height: innerShorter,
+        left: xIndex * innerLonger,
+        top: yIndex * innerShorter,
+        width: innerLonger,
+      });
+    }
+  }
+  if (verticalCounter) {
+    for (let xIndex = 0; xIndex < verticalCounter; xIndex++) {
+      items.push({
+        height: innerLonger,
+        left: xHorizontalCounter * innerLonger + xIndex * innerShorter,
+        top: 0,
+        width: innerShorter,
+      });
+    }
+  }
+
+  return {
+    horizontal: horizontalCounter,
+    items,
+    total: horizontalCounter + verticalCounter,
+    vertical: verticalCounter,
+  };
+};
+
+export default function Home(): JSX.Element {
+  const roofElRef = useRef<HTMLDivElement>(null);
+  const roofWidthInputRef = useRef<HTMLInputElement>(null);
+  const graphElRef = useRef<HTMLDivElement>(null);
+  const [roofDimensions, setRoofDimensions] = useState<{
+    left: number;
+    top: number;
+    height: number;
+    width: number;
+  }>({
+    left: 0,
+    top: 0,
+    height: 0,
+    width: 0,
+  });
+  const [roofWidth, setRoofWidth] = useState(0);
+  const [roofHeight, setRoofHeight] = useState(0);
+  const [solarPanelWidth, setSolarPanelWidth] = useState(0);
+  const [solarPanelHeight, setSolarPanelHeight] = useState(0);
+  const [result, setResult] = useState<null | InnerRectangles>(null);
+
+  const onCalculateClick = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (result !== null) {
+      // Reset values and remove result
+      setRoofWidth(0);
+      setRoofHeight(0);
+      setSolarPanelWidth(0);
+      setSolarPanelHeight(0);
+      setResult(null);
+      return;
+    }
+
+    setResult(
+      calculateRectangles(
+        roofWidth,
+        roofHeight,
+        solarPanelWidth,
+        solarPanelHeight
+      )
+    );
+  };
+
+  // Get roof dimensions
+  useEffect(() => {
+    if (result !== null && result.total > 0 && roofElRef.current) {
+      const { left, top, width } = roofElRef.current.getBoundingClientRect();
+      setRoofDimensions({
+        left,
+        top,
+        width,
+        height: (width * roofHeight) / roofWidth,
+      });
+    }
+  }, [result, roofWidth, roofHeight]);
+
+  // Set input focus on reset
+  useEffect(() => {
+    if (roofWidthInputRef.current && !result) {
+      roofWidthInputRef.current.select();
+      roofWidthInputRef.current.focus();
+    }
+  }, [result, roofWidthInputRef]);
+
+  // Set scroll on calculation
+  useEffect(() => {
+    if (result !== null && result.total > 0 && graphElRef.current) {
+      // Wait until rectangles are created into DOM before scrolling
+      setTimeout(() => {
+        graphElRef?.current?.scrollIntoView({ behavior: "smooth" });
+      }, 200);
+    }
+  }, [result, graphElRef]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen min-w-80 flex-col max-w-5xl justify-start p-6 sm:p-12 sm:mx-auto md:p-24">
+      <h1 className="text-3xl mb-6 text-center sm:text-4xl">
+        Calculadora de Paneles Solares
+      </h1>
+      <p className="mb-8 text-justify">
+        Introduzca las dimensiones del tejado y de los paneles solares para que
+        el sistema calcule por usted cuántos paneles solares podría instalar en
+        un tejado según las dimensiones dadas.
+      </p>
+
+      <form
+        className="flex flex-col"
+        onSubmit={($event) => onCalculateClick($event)}
+      >
+        <div id="roof" className="mb-3">
+          <h3 className="font-bold text-primary mb-3">Tejado</h3>
+
+          <div className="flex flex-col gap-4 sm:flex-row">
+            <div
+              id="roofWidthContainer"
+              className="flex flex-row flex-1 items-center"
+            >
+              <label htmlFor="roofWidth">Ancho:</label>
+              <input
+                type="number"
+                name="roofWidth"
+                id="roofWidth"
+                ref={roofWidthInputRef}
+                className="border-b-2 flex-1 outline-none focus:border-b-primary mx-2"
+                value={roofWidth}
+                disabled={result !== null}
+                onChange={($event) => setRoofWidth(+$event.target.value)}
+              />
+              cm
+            </div>
+
+            <div
+              id="roofHeightContainer"
+              className="flex flex-row flex-1 items-center"
+            >
+              <label htmlFor="roofHeight">Alto:</label>
+              <input
+                type="number"
+                name="roofHeight"
+                id="roofHeight"
+                className="border-b-2 flex-1 outline-none focus:border-b-primary mx-2"
+                value={roofHeight}
+                disabled={result !== null}
+                onChange={($event) => setRoofHeight(+$event.target.value)}
+              />
+              cm
+            </div>
+          </div>
         </div>
-      </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div id="solarPanel" className="mb-3">
+          <h3 className="font-bold text-primary mb-3">Paneles solares</h3>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+          <div className="flex flex-col gap-4 sm:flex-row">
+            <div
+              id="solarPanelWidthContainer"
+              className="flex flex-row flex-1 items-center"
+            >
+              <label htmlFor="solarPanelWidth">Ancho:</label>
+              <input
+                type="number"
+                name="solarPanelWidth"
+                id="solarPanelWidth"
+                className="border-b-2 flex-1 outline-none focus:border-b-primary mx-2"
+                value={solarPanelWidth}
+                disabled={result !== null}
+                onChange={($event) => setSolarPanelWidth(+$event.target.value)}
+              />
+              cm
+            </div>
+
+            <div
+              id="solarPanelHeightContainer"
+              className="flex flex-row flex-1 items-center"
+            >
+              <label htmlFor="solarPanelHeight">Alto:</label>
+              <input
+                type="number"
+                name="solarPanelHeight"
+                id="solarPanelHeight"
+                className="border-b-2 flex-1 outline-none focus:border-b-primary mx-2"
+                value={solarPanelHeight}
+                disabled={result !== null}
+                onChange={($event) => setSolarPanelHeight(+$event.target.value)}
+              />
+              cm
+            </div>
+          </div>
+        </div>
+
+        <button
+          id="calculateButton"
+          type="submit"
+          className="px-3 py-2 rounded-xl bg-primary text-white mt-5 disabled:bg-disabled sm:mx-auto sm:w-fit min-w-36"
+          disabled={
+            !(roofWidth && roofHeight && solarPanelWidth && solarPanelHeight)
+          }
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          {result !== null ? "Reiniciar" : "Calcular"}
+        </button>
+      </form>
+      {result !== null && (
+        <div className="flex flex-col" ref={graphElRef}>
+          <div className="text-center mt-6">
+            <p>
+              Nº de Paneles Solares:{" "}
+              <span className="text-primary font-bold">{result.total}</span>.
+            </p>
+            <p>
+              De los cuales{" "}
+              <span className="text-primary font-bold">
+                {result.horizontal}
+              </span>{" "}
+              están en horizontal y{" "}
+              <span className="text-primary font-bold">{result.vertical}</span>{" "}
+              en vertical.
+            </p>
+          </div>
+          {result.total > 0 && (
+            <div className="flex flex-col gap-1 mt-6">
+              <h2 className="md:text-lg">Visualización</h2>
+              <div
+                ref={roofElRef}
+                className="bg-slate-100 w-full relative text-white"
+                style={{ height: roofDimensions.height }}
+              >
+                {result.items.map((solarPanel, index) => (
+                  <div
+                    key={index}
+                    className="absolute flex justify-center items-center text-center"
+                    style={{
+                      left:
+                        (roofDimensions.width / roofWidth) * solarPanel.left,
+                      top: (roofDimensions.width / roofWidth) * solarPanel.top,
+                      height:
+                        (roofDimensions.width / roofWidth) * solarPanel.height,
+                      width:
+                        (roofDimensions.width / roofWidth) * solarPanel.width,
+                      backgroundColor: hexRandomColor(),
+                    }}
+                  >
+                    Panel Nº{index + 1}
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
+      )}
     </main>
   );
 }
